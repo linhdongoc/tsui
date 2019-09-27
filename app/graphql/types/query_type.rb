2d0eb6d2909c0cf0c6d0669707faff8b7f37db58
Linh-Hoa Do-Ngoc <linhhoa.do@gmail.com>
2019-09-27 11:16:43 +0200

    #TSUI-1: add model user and book

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,8 +1,17 @@
 module Types
-  QueryType = GraphQL::ObjectType.new.tap do |root_type|
-    root_type.name = 'Query'
-    root_type.description = 'The query root'
-    root_type.interfaces = []
-    root_type.fields = Util::FieldCombiner.combine([QueryType::TodoListQueryType])
+  class QueryType < Types::BaseObject
+    field :users, [Types::UserType], null: false
+
+    def users
+      User.all
+    end
+
+    field :user, Types::UserType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def user(id:)
+      User.find(id)
+    end
   end
 end
